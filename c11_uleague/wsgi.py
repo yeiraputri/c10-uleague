54a720113008233648bf67fd40607ed754d8ec79
yeiraputri <112608722+yeiraputri@users.noreply.github.com>
2023-06-17 16:59:35 +0700

    Update wsgi.py

--- c11_uleague/wsgi.py
+++ c11_uleague/wsgi.py
@@ -9,6 +9,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "c11_uleague.settings")
 application = get_wsgi_application()
 app = application
